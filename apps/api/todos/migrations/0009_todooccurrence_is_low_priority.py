9c9e72aea5f1886b8a15c77e79223623de1f106d
Ryan T <trh991015@gmail.com>
2026-06-22 21:46:04 +0800

    Stabilize low priority migration deploy

--- apps/api/todos/migrations/0009_todooccurrence_is_low_priority.py
+++ apps/api/todos/migrations/0009_todooccurrence_is_low_priority.py
@@ -1,12 +1,23 @@
 from django.db import migrations, models
 
 
+def disable_postgres_statement_timeout(apps, schema_editor):
+    if schema_editor.connection.vendor != "postgresql":
+        return
+    with schema_editor.connection.cursor() as cursor:
+        cursor.execute("SET statement_timeout = 0")
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("todos", "0008_repair_occurrence_attachment_task"),
     ]
 
     operations = [
+        migrations.RunPython(
+            disable_postgres_statement_timeout,
+            reverse_code=migrations.RunPython.noop,
+        ),
         migrations.AddField(
             model_name="todooccurrence",
             name="is_low_priority",
