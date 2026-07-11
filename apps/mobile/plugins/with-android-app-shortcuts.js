b1505e35bd7d3cea869870a755f278cca81f75bf
Ryan T <trh991015@gmail.com>
2026-07-12 07:59:43 +0800

    Fix Android shortcuts and atomic image pushes

--- apps/mobile/plugins/with-android-app-shortcuts.js
+++ apps/mobile/plugins/with-android-app-shortcuts.js
@@ -9,22 +9,31 @@ const {
 const SHORTCUTS_XML = `<?xml version="1.0" encoding="utf-8"?>
 <shortcuts xmlns:android="http://schemas.android.com/apk/res/android">
   <shortcut android:shortcutId="add_task" android:enabled="true"
-    android:icon="@mipmap/ic_launcher" android:shortcutShortLabel="添加任务"
-    android:shortcutLongLabel="快速添加任务">
+    android:icon="@mipmap/ic_launcher" android:shortcutShortLabel="@string/shortcut_add_task"
+    android:shortcutLongLabel="@string/shortcut_add_task_long">
     <intent android:action="android.intent.action.VIEW"
       android:targetPackage="com.dailytodosync.app"
       android:targetClass="com.dailytodosync.app.MainActivity"
       android:data="daily-todo://today?compose=1" />
   </shortcut>
   <shortcut android:shortcutId="add_expense" android:enabled="true"
-    android:icon="@mipmap/ic_launcher" android:shortcutShortLabel="记一笔"
-    android:shortcutLongLabel="快速手工记账">
+    android:icon="@mipmap/ic_launcher" android:shortcutShortLabel="@string/shortcut_add_expense"
+    android:shortcutLongLabel="@string/shortcut_add_expense_long">
     <intent android:action="android.intent.action.VIEW"
       android:targetPackage="com.dailytodosync.app"
       android:targetClass="com.dailytodosync.app.MainActivity"
       android:data="daily-todo://expenses?manual=1" />
   </shortcut>
 </shortcuts>
+`;
+
+const SHORTCUT_STRINGS_XML = `<?xml version="1.0" encoding="utf-8"?>
+<resources>
+  <string name="shortcut_add_task">添加任务</string>
+  <string name="shortcut_add_task_long">快速添加任务</string>
+  <string name="shortcut_add_expense">记一笔</string>
+  <string name="shortcut_add_expense_long">快速手工记账</string>
+</resources>
 `;
 
 module.exports = function withAndroidAppShortcuts(config) {
@@ -47,6 +56,9 @@ module.exports = function withAndroidAppShortcuts(config) {
     const xmlDir = path.join(modConfig.modRequest.platformProjectRoot, "app", "src", "main", "res", "xml");
     fs.mkdirSync(xmlDir, { recursive: true });
     fs.writeFileSync(path.join(xmlDir, "shortcuts.xml"), SHORTCUTS_XML);
+    const valuesDir = path.join(modConfig.modRequest.platformProjectRoot, "app", "src", "main", "res", "values");
+    fs.mkdirSync(valuesDir, { recursive: true });
+    fs.writeFileSync(path.join(valuesDir, "shortcut_strings.xml"), SHORTCUT_STRINGS_XML);
     return modConfig;
   }]);
 };
